37e6eb195be7c23aaf08b316d2ee279b68b05764
varkashy <varunkkashyap@gmail.com>
2018-05-24 22:32:58 -0500

    Spring bus - dynamic refresh - ConfigurationProperties

--- src/main/java/com/spring/cloud/config/application/LuckyWordController.java
+++ src/main/java/com/spring/cloud/config/application/LuckyWordController.java
@@ -1,19 +1,31 @@
 package com.spring.cloud.config.application;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @RestController
-@RefreshScope
+@ConfigurationProperties
 public class LuckyWordController {
 
-    @Value("${lucky-word}")
     String luckyWord;
 
+    /*@Value("${lucky-word}")
+    String luckyWord;*/
+
     @GetMapping("/lucky-word")
-    public String getLuckyWord(){
+    public String showLuckyWord(){
         return "the lucky word is "+luckyWord;
+    }
+    public String getLuckyWord() {
+        return luckyWord;
+    }
+
+    public void setLuckyWord(String luckyWord) {
+        this.luckyWord = luckyWord;
     }
 }
