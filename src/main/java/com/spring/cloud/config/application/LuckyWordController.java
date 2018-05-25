a50eb18656a7222699493cc9214d8308a670d1d3
varkashy <varunkkashyap@gmail.com>
2018-05-24 21:48:45 -0500

    Spring bus - dynamic refresh

--- src/main/java/com/spring/cloud/config/application/LuckyWordController.java
+++ src/main/java/com/spring/cloud/config/application/LuckyWordController.java
@@ -1,10 +1,12 @@
 package com.spring.cloud.config.application;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class LuckyWordController {
 
     @Value("${lucky-word}")
